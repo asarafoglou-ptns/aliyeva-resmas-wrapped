30222de8b13cfcd3f25d06b1b3ac466b1b2d5665
ofelyaaa <157810991+ofelyaaa@users.noreply.github.com>
2024-05-27 07:16:19 +0200

    added a page and functions that create radar plots with audio features

--- myapp/app.py
+++ myapp/app.py
@@ -1,55 +1,113 @@
 import streamlit as st
-from shared import df
-import os
+import plotly.graph_objects as go
+from shared import df, df_combined  # Import the DataFrames from shared.py
 
-# Page layout, reminder: must be the first st. command in script
+# page layout, reminder: must be the first st. command in script
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 
-# create relative path to styles.css file
-css_path = os.path.join(os.getcwd(), 'static', 'styles.css')
-
+# read and apply CSS styles - will change to relative path in a later commit
+css_path = '/Users/ofelyaaliyeva/Desktop/aliyeva-resmas-wrapped/static/styles.css'
 with open(css_path) as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    css = f.read()
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-# page title
-st.title("Your Top Songs")
+# navigation
+page = st.sidebar.selectbox("Choose a page", ["Your Top Songs", "Audio Features Analysis"])
 
-# create a sidebar to select time period and number of albums
-st.sidebar.markdown("""
-    <style>
-    * {
-        font-family: 'Helvetica', sans-serif;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+if page == "Your Top Songs":
 
-st.sidebar.title("Filter")
-time_period = st.sidebar.selectbox("Select time period", df['time_period'].unique(), key="time_period")
-num_songs = st.sidebar.slider("Select number of top songs", min_value=1, max_value=20, value=6, key="num_songs")
+    st.title("Your Top Songs")
 
-def get_filtered_df(time_period, num_songs):
-    return df[df['time_period'] == time_period].head(num_songs)
+    # sidebar
+    st.sidebar.title("Filter")
+    time_period = st.sidebar.selectbox("Select time period", df['time_period'].unique(), key="time_period")
+    num_songs = st.sidebar.slider("Select number of top songs", min_value=1, max_value=30, value=6, key="num_songs")
+    search_query = st.sidebar.text_input("Search for a song or artist")
 
+    def get_filtered_df(time_period, num_songs, search_query):
+        filtered_df = df[df['time_period'] == time_period]
+        if search_query:
+            filtered_df = filtered_df[
+                filtered_df['name'].str.contains(search_query, case=False, na=False) |
+                filtered_df['artist'].str.contains(search_query, case=False, na=False)
+            ]
+        return filtered_df.head(num_songs)
 
-# filter the DataFrame based on the selected time period and number of top songs
-filtered_df = get_filtered_df(time_period, num_songs)
+    # filters the dataframe based on the selected time period, number of top songs, and search query
+    filtered_df = get_filtered_df(time_period, num_songs, search_query)
 
-image_height = 400
+    image_height = 400
 
-# Display the songs in a vertical list with larger images
-for i in range(0, len(filtered_df), 3):
-    cols = st.columns(3)
-    for col, row in zip(cols, filtered_df.iloc[i:i+3].iterrows()):
-        index, song = row
-        with col:
-            st.markdown(f"""
-                <div class="song-container">
-                    <img src="{song['album_cover']}" class="song-image" width="{image_height}" height="{image_height}">
-                    <div>
-                        <p class='song-info'><strong>Song:</strong> <a href='{song['spotify_url']}' target='_blank'>{song['name']}</a></p>
-                        <p class='song-info'><strong>Artist:</strong> {song['artist']}</p>
-                        <p class='song-info'><strong>Album:</strong> {song['album']}</p>
+    # displays the songs in a vertical list with large images
+    for i in range(0, len(filtered_df), 3):
+        cols = st.columns(3)
+        for col, row in zip(cols, filtered_df.iloc[i:i+3].iterrows()):
+            index, song = row
+            with col:
+                st.markdown(f"""
+                    <div class="song-container">
+                        <img src="{song['album_cover']}" class="song-image" width="{image_height}" height="{image_height}">
+                        <div>
+                            <p class='song-info'><strong>Song:</strong> <a href='{song['spotify_url']}' target='_blank'>
+                            {song['name']}</a></p>
+                            <p class='song-info'><strong>Artist:</strong> {song['artist']}</p>
+                            <p class='song-info'><strong>Album:</strong> {song['album']}</p>
+                        </div>
                     </div>
-                </div>
-                <hr class="separator">
-            """, unsafe_allow_html=True)
+                    <hr class="separator">
+                """, unsafe_allow_html=True)
+
+elif page == "Audio Features Analysis":
+    st.title("Audio Features Analysis")
+
+    # Sidebar options for selecting features to plot
+    st.sidebar.title("Select Features for Radar Chart")
+    features = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+    selected_features = [feature for feature in features if st.sidebar.checkbox(feature, True)]
+
+    # selects data to display
+    data_option = st.radio("Select data to display", ("User's Top Tracks", "Global Top 50", "Both"), horizontal=True)
+
+    if selected_features:
+        # Filter the combined DataFrame based on the selected features and data option
+        if data_option == "Both":
+            filtered_combined_df = df_combined[df_combined['feature'].isin(selected_features)]
+        elif data_option == "Global Top 50":
+            filtered_combined_df = df_combined[(df_combined['feature'].isin(selected_features)) & (df_combined['group'] == "Global Top 50")]
+        else:
+            filtered_combined_df = df_combined[(df_combined['feature'].isin(selected_features)) & (df_combined['group'] == "User")]
+
+
+        custom_colors = ['#FF6347', '#32CD32', '#1E90FF']
+
+        # creates radar chart
+        fig = go.Figure()
+
+        for idx, group in enumerate(filtered_combined_df['group'].unique()):
+            group_df = filtered_combined_df[filtered_combined_df['group'] == group]
+            fig.add_trace(go.Scatterpolar(
+                r=group_df['value'],
+                theta=group_df['feature'],
+                fill='toself',
+                name=group,
+                line=dict(color=custom_colors[idx % len(custom_colors)])
+            ))
+
+        fig.update_layout(
+            polar=dict(
+                bgcolor='#000000',
+                radialaxis=dict(visible=True, range=[0, 1], showline=False, ticks=''),
+                angularaxis=dict(visible=True, showline=False, ticks='')
+            ),
+            showlegend=True,
+            plot_bgcolor='#000000',
+            paper_bgcolor='#000000',
+            font=dict(color='#ffffff'),
+            height=700,
+            width=700
+        )
+
+        st.subheader("Radar Chart of Selected Features (Normalized)")
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.write("Please select at least one feature to display the radar chart.")
--- myapp/shared.py
+++ myapp/shared.py
@@ -1,8 +1,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-# my spotify API credentials
+# Spotify API credentials - you can use mine for testing
 SPOTIPY_CLIENT_ID = '4e6464520c2b4cf3a90ad792e5ad6504'
 SPOTIPY_CLIENT_SECRET = '4dde9d4fbb604ad3b22c0ad7204e25a0'
 SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
@@ -14,75 +15,150 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                redirect_uri=SPOTIPY_REDIRECT_URI,
                                                scope=SCOPE))
 
-
 class GetTrack:
     """
     A class with functions that retrieve various information about tracks (for now: ID and features).
     """
 
     @staticmethod
-    def get_track_ids(time_frame):
+    def get_track_ids(tracks, is_playlist=False):
         """
         Returns a list of track IDs.
 
-        :param time_frame: short, medium, and long-term listening habits
-        :type time_frame: dict
+        :param tracks: List of track objects
+        :type tracks: dict
+        :param is_playlist: Boolean indicating if the tracks are from a playlist
+        :type is_playlist: bool
         :return: A list of track IDs
         """
-        return [song['id'] for song in time_frame['items']]
+        if is_playlist:
+            return [track['track']['id'] for track in tracks['items']]
+        else:
+            return [track['id'] for track in tracks['items']]
 
     @staticmethod
-    def get_track_features(sp, track_id):
-        """
-        Retrieves track information - name of the song, album, artist
-        Retrieves track information - name of the song, album, artist, spotify url, and album cover.
-        """
-        meta = sp.track(track_id)
-        name = meta['name']
-        album = meta['album']['name']
-        artist = meta['album']['artists'][0]['name']
-        spotify_url = meta['external_urls']['spotify']
-        album_cover = meta['album']['images'][0]['url']
-        track_info = [name, album, artist, spotify_url, album_cover]
-        return track_info
-
-    @staticmethod
-    def get_multiple_track_features(sp, track_ids):
+    def get_track_features(sp, track_ids):
         """
         Retrieves track information for multiple tracks in a batch request.
         """
         tracks_meta = sp.tracks(track_ids)
-        track_features = []
-        for meta in tracks_meta['tracks']:
-            name = meta['name']
-            album = meta['album']['name']
-            artist = meta['album']['artists'][0]['name']
-            spotify_url = meta['external_urls']['spotify']
-            album_cover = meta['album']['images'][0]['url']
-            track_info = [name, album, artist, spotify_url, album_cover]
-            track_features.append(track_info)
+        track_features = [
+            [
+                meta['name'],
+                meta['album']['name'],
+                meta['album']['artists'][0]['name'],
+                meta['external_urls']['spotify'],
+                meta['album']['images'][0]['url']
+            ]
+            for meta in tracks_meta['tracks']
+        ]
         return track_features
 
-def collect_tracks_data(sp, time_ranges):
-    all_tracks = []
+    @staticmethod
+    def get_audio_features(sp, track_ids):
+        """
+        Retrieves audio features for multiple tracks in a batch request.
+        """
+        audio_features = sp.audio_features(track_ids)
+        return audio_features
 
-    for time_period in time_ranges:
-        top_tracks = sp.current_user_top_tracks(limit=20, offset=0, time_range=time_period)
-        track_ids = GetTrack.get_track_ids(top_tracks)
+    @staticmethod
+    def collect_tracks_data(sp, source, label, is_playlist=False):
+        """
+        Collects track data (both metadata and audio features) from a specified source.
+
+        :param sp: Spotipy client
+        :param source: Function to retrieve tracks (e.g., sp.current_user_top_tracks, sp.playlist_tracks)
+        :param label: Label indicating the source or time range
+        :return: List of combined track data
+        """
+        tracks = source()
+        track_ids = GetTrack.get_track_ids(tracks, is_playlist)
 
         # Fetch track features in batch
-        track_features = GetTrack.get_multiple_track_features(sp, track_ids)
-        for track in track_features:
-            track.append(time_period)  # Add the time period as the last element
-            all_tracks.append(track)
+        track_features = GetTrack.get_track_features(sp, track_ids)
 
-    return all_tracks
+        # Fetch audio features in batch
+        audio_features = GetTrack.get_audio_features(sp, track_ids)
+
+        all_tracks = [
+            track + [
+                audio_feature['danceability'],
+                audio_feature['energy'],
+                audio_feature['key'],
+                audio_feature['loudness'],
+                audio_feature['mode'],
+                audio_feature['speechiness'],
+                audio_feature['acousticness'],
+                audio_feature['instrumentalness'],
+                audio_feature['liveness'],
+                audio_feature['valence'],
+                audio_feature['tempo'],
+                audio_feature['duration_ms'],
+                audio_feature['time_signature'],
+                label
+            ]
+            for track, audio_feature in zip(track_features, audio_features)
+        ]
+
+        return all_tracks
 
 
-time_ranges = ['short_term', 'medium_term', 'long_term']
+# maps user-friendly labels onto the time periods provided by spotify
+time_ranges = {
+    'short_term': 'Past month',
+    'medium_term': 'Past 6 months',
+    'long_term': 'Past year'
+}
 
-# Use our function to get the track data
-tracks_data = collect_tracks_data(sp, time_ranges)
+# function to fetch user's top tracks for a given time range
+def fetch_user_top_tracks(time_range):
+    return sp.current_user_top_tracks(limit=30, offset=0, time_range=time_range)
 
-# Create a pandas DataFrame with the data
-df = pd.DataFrame(tracks_data, columns=['name', 'album', 'artist', 'spotify_url', 'album_cover', 'time_period'])
+# use our function to get the user's top tracks' data
+user_tracks_data = [
+    track for time_range, label in time_ranges.items()
+    for track in GetTrack.collect_tracks_data(sp, lambda tr=time_range: fetch_user_top_tracks(tr), label)
+]
+
+# use our function to get data for the global top 50
+global_top_50_data = GetTrack.collect_tracks_data(
+    sp, lambda: sp.playlist_items('37i9dQZEVXbMDoHDwVN2tF', limit=50, offset=0), 'Global Top 50', is_playlist=True)
+
+# creates a dataframe with the user's top tracks data
+columns = ['name', 'album', 'artist', 'spotify_url', 'album_cover', 'danceability', 'energy', 'key', 'loudness', 'mode',
+           'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms',
+           'time_signature', 'time_period']
+df = pd.DataFrame(user_tracks_data, columns=columns)
+
+# creates a dataframe with the global top 50 tracks data
+df_global_top_50 = pd.DataFrame(global_top_50_data, columns=columns)
+
+# add a 'source' column to each DataFrame
+df['source'] = 'User'
+df_global_top_50['source'] = 'Global Top 50'
+
+# merge the two dataframes
+df_merged = pd.concat([df, df_global_top_50], ignore_index=True)
+
+# normalize the audio features so they can be compared
+features = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+scaler = MinMaxScaler()
+df_merged[features] = scaler.fit_transform(df_merged[features])
+
+# more data prep. for comparison
+def prepare_comparison_data(df, features, label):
+    comparison_data = df[df['source'] == label][features].mean().reset_index()
+    comparison_data.columns = ['feature', 'value']
+    comparison_data['group'] = label
+    return comparison_data
+
+comparison_df_user = prepare_comparison_data(df_merged, features, 'User')
+comparison_df_global = prepare_comparison_data(df_merged, features, 'Global Top 50')
+
+# combine comparison data
+df_combined = pd.concat([comparison_df_user, comparison_df_global], ignore_index=True)
+
+# Display the prepared data for comparison
+print("\nPrepared Data for Comparison:")
+print(df_combined)
