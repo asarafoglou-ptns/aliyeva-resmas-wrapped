3dc4c1a2596c021d0728eb970d048cae26a054ad
ofelyaaa <157810991+ofelyaaa@users.noreply.github.com>
2024-05-31 16:46:12 +0200

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,18 @@ setuptools.setup(
     version="0.0.1",
     author="ofelya",
     author_email="ofelyaaaliyeva@gmail.com",
-    python_requires=">3.6",
-    package_data={"": ["requirements.txt"]},
-    packages=setuptools.find_packages()
+    description="Description of your package",
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "spotipy~=2.23.0",
+        "streamlit~=1.35.0",
+        "faicons",
+        "shiny",
+        "seaborn",
+        "pandas~=2.0.3",
+        "plotly~=5.9.0",
+        "scikit-learn~=1.3.0",
+        "setuptools~=68.0.0",
+    ],
 )
